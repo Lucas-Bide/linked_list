8cc3e3344d5a9e7194158aad96a3be1b4665befe
Lucas Bide <lucas.bidemachado@gmail.com>
2020-04-03 20:21:49 -0400

    Complete

---
+++ node.rb
@@ -0,0 +1,18 @@
+class Node
+  attr_accessor :value, :next_node
+'''
+  def initialize(value, next_node)
+    @value = value
+    @next_node = next_node
+  end
+'''
+  def initialize(value)
+    @value = value
+    @next_node = nil
+  end
+'''
+  def initialize
+    initialize(nil, nil)
+  end
+  '''
+end
